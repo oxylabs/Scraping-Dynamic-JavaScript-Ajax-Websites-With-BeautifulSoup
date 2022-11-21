8297f0e415c88319b1e8172bf82448cb2424c29e
oxyjohan <95211181+oxyjohan@users.noreply.github.com>
2022-11-21 11:08:45 +0200

    Update selenium_example.py

--- selenium_example.py
+++ selenium_example.py
@@ -1,4 +1,5 @@
 from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 # update executable_path as required
@@ -8,11 +9,11 @@ driver.get('https://quotes.toscrape.com/js/')
 
 try:
     # print first author
-    author_element = driver.find_element_by_tag_name("small")
+    author_element = driver.find_element(By.TAG_NAME, "small")
     print(author_element.text)
 
     # print all authors
-    all_author_elements = driver.find_elements_by_tag_name("small")
+    all_author_elements = driver.find_elements(By.TAG_NAME, "small")
     for element in all_author_elements:
         print(element.text)
 finally:
